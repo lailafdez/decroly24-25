94fa8630f1dbc308b66fd22d4991c1e206301f2f
COMPU-S021\daw1 <lailafdezs135@gmail.com>
2024-10-21 14:28:22 +0200

    no message

--- ACTIVIDAD4/src/AC4/AC4.java
+++ ACTIVIDAD4/src/AC4/AC4.java
@@ -97,12 +97,86 @@ public class AC4 {
 	   		}
 	   		
 	   		for(int i = 0; i < num4.length; i++) {
-	   			
+	   			suma += num4 [i];
+	   			}
+	   		System.out.println("La media de la suma de los numeros es: " + (suma/num4.length));
+	   		
+	   //Ejercicio6
+	   System.out.println("Escribe el numero del tamaño del array: ");
+	   Scanner reader5 = new Scanner(System.in);
+	   int N = reader5.nextInt();
+	   System.out.println("Escribe el numero para llenar el array: ");
+	   Scanner reader6 = new Scanner(System.in);
+	   int M = reader6.nextInt();
+	   int [] num5 = new int [N];
+	   		for (int i = 0; i < N; i++) {
+	   			num5[i] = M;
 	   		}
+	   System.out.println("El array es: ");
+	   		for (int value : num5) {
+	   			System.out.println(value + " ");
+	   		}
+	   		
+	   //Ejercicio7
+	   System.out.println("Escribe  numero que sera P: ");
+	   Scanner reader7 = new Scanner(System.in);
+	   int P = reader7.nextInt();
+	   System.out.println("Escribe numero mayor que P que sera Q: ");
+	   Scanner reader8 = new Scanner(System.in);
+	   int Q = reader8.nextInt();
+	   int array;
+	   if (P <= Q) {
+		   array = Q - P + 1;
+	   }
+	   else {
+		   array = P - Q + 1;
+	   }
+	   int [] num6 = new int [array];
+	   for ( int i = 0; i < array; i++) {
+		   if ( P <= Q ) {
+			   num6[i] = P + i;
+		   }
+		   if ( Q <= P ) {
+			   num6[i] = Q + i;
+		   }
+	   }
+	   System.out.println(" El array es: "); 
+	   for (int value : num6) {
+		   System.out.println(value + " ");
+	   }
+	   
+	  //Ejercicio8 
+	   double[] num7 = new double[100];
+		   for (int i = 1; i < num7.length; i++) {
+		   num7[i] = Math.random();
+		   }
+	           Scanner scanner = new Scanner(System.in);
+	           System.out.print("Escribe un numero real entre 0,0 y 1,0 que sera R: ");
+	           double R = scanner.nextDouble();
+
+	           int igualsup = 0;
+	           for (double value : num7) {
+	               if (value >= R) {
+	                   igualsup++;
+	               }
+	           }
+	           System.out.println("Numero de valores en el array iguales o superiores a R: " + igualsup);
+	  
+	  //Ejercicio9
+		int num8 = 100;
+		int[] array1 = new int[num8];
+		for ( int i = 0; i < array1.length; i++) {
+		array1 [i] =  1 + (int)  Math.random() * 10;
+		}
+		 System.out.println("Escribe un numero que sera N: ");
+	
+	
 	
 	
 	}
 }
+
+
 
 		
 	
