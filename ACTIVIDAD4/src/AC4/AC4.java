6fdcdc4281f7c027235d4e7fce1bc301f3f83ecb
COMPU-S021\daw1 <lailafdezs135@gmail.com>
2024-10-23 12:27:07 +0200

    no message

--- ACTIVIDAD4/src/AC4/AC4.java
+++ ACTIVIDAD4/src/AC4/AC4.java
@@ -7,7 +7,6 @@ public class AC4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
 		//Ejercicio 1
 		double [] num = new double [10]; 
 		Scanner reader = new Scanner(System.in);
@@ -161,20 +160,153 @@ public class AC4 {
 	               }
 	           }
 	           System.out.println("Numero de valores en el array iguales o superiores a R: " + igualsup);
-	  
 	  //Ejercicio9
-		int num8 = 100;
-		int[] array1 = new int[num8];
-		for ( int i = 0; i < array1.length; i++) {
-		array1 [i] =  1 + (int)  Math.random() * 10;
-		}
-		 System.out.println("Escribe un numero que sera N: ");
-	
-	
-	
-	
+	  //Ejercicio10
+	           Scanner scanner1 = new Scanner(System.in);
+               System.out.print("Introduce el número de alturas a registrar (N): ");
+               int N1 = scanner1.nextInt();
+
+               double[] alturas = new double[N1];
+               for (int i = 0; i < N1; i++) {
+                   System.out.print("Introduce la altura de la persona " + (i + 1) + " (en metros): ");
+                   alturas[i] = scanner1.nextDouble();
+               }
+               double suma1 = 0;
+               double max = alturas[0];
+               double min = alturas[0];
+
+               for (double altura : alturas) {
+                   suma1 += altura;
+                   if (altura > max) {
+                       max = altura;
+                   }
+                   if (altura < min) {
+                       min = altura;
+                   }
+               }
+
+               double media = suma1 / N1;
+               int porEncima = 0;
+               int porDebajo = 0;
+
+               for (double altura : alturas) {
+                   if (altura > media) {
+                       porEncima++;
+                   } else if (altura < media) {
+                       porDebajo++;
+                   }
+               }
+               System.out.printf("Altura media:  " + media 	 );
+               System.out.printf("Altura máxima:  " + max );
+               System.out.printf("Altura mínima: " + min );
+               System.out.println("Número de personas por encima de la media: " + porEncima );
+               System.out.println("Número de personas por debajo de la media: " + porDebajo );
+
+           
+     //Ejercicio11
+            int[] array2 = new int[100];
+   	        int[] array3 = new int[100];
+   	        for (int i = 0; i < array2.length; i++) {
+   	            array2[i] = i + 1;
+   	        }
+   	        for (int i = 0; i < array2.length; i++) {
+   	            array3[i] = array2[array2.length - 1 - i]; 
+   	        }
+   	        System.out.print("Del 1 al 100: ");
+   	        for (int num11 : array2) {
+   	            System.out.print(num11 + " ");
+   	        }
+   	        System.out.println();
+
+   	        System.out.print("Del 100 al 1 (inverso): ");
+   	        for (int num11 : array3) {
+   	            System.out.print(num11 + " ");
+   	        }
+   	        System.out.println();
+       
+       	//Ejercicio12
+       	          Scanner scanner2 = new Scanner(System.in);
+       	          int[] array4 = new int[10]; 
+       	          char opcion;
+
+       	          do {
+       	              System.out.println("Seleccione una opción:");
+       	              System.out.println("a. Mostrar valores.");
+       	              System.out.println("b. Introducir valor.");
+       	              System.out.println("c. Salir.");
+       	              opcion = scanner2.next().charAt(0);
+
+       	              if (opcion == 'a') {
+       	                  System.out.println("Valores en el array:");
+       	                  for (int i = 0; i < array4.length; i++) {
+       	                      System.out.println("Posición " + i + ": " + array4[i]);
+       	                  }
+       	              } else if (opcion == 'b') {
+       	                  System.out.print("Escribe un numero (entero): ");
+       	                  int valor = scanner2.nextInt();
+       	                  System.out.print("Introduzca una posición (0-9): ");
+       	                  int posicion = scanner2.nextInt();
+
+       	                  if (posicion >= 0 && posicion < array4.length) {
+       	                      array4[posicion] = valor;
+       	                      System.out.println("Valor " + valor + " añadido en la posición " + posicion);
+       	                  } else {
+       	                      System.out.println("Error, debe estar entre 0 y 9.");
+       	                  }
+       	              } else if (opcion != 'c') {
+       	                  System.out.println("Opción no válida. Intentalo de nuevo.");
+       	              }
+
+       	          } while (opcion != 'c');
+
+       	          System.out.println("Saliste del programa");
+       	          scanner2.close();
+     
+
+       	         //Ejercicio13
+       	            Scanner scanner3 = new Scanner(System.in);
+
+       	            System.out.print("Introduzca el valor inicial (V): ");
+       	            int V = scanner3.nextInt();
+
+       	            System.out.print("Introduzca el incremento (I): ");
+       	            int I = scanner3.nextInt();
+
+       	            System.out.print("Escribe el número de valores a crear (N): ");
+       	            int N2 = scanner3.nextInt();
+       	            
+       	            int[] secuencia = new int[N2];
+       	            for (int i = 0; i < N2; i++) {
+       	                secuencia[i] = V + (i * I);
+       	            }
+       	            
+       	            System.out.println("La secuencia aritmética es: ");
+       	            for (int num13 : secuencia) {
+       	                System.out.print(num13 + " ");
+       	            }
+
+       	            scanner3.close();
+       	       
+
+       	//Ejercicio14
+  	            int[] secuencia1 = new int[55]; 
+  	            int num12 = 0;
+
+  	            for (int i = 1; i <= 10; i++) {
+  	                for (int j = 0; j < i; j++) {
+  	                    secuencia[num12++] = i;
+  	                }
+  	            }
+
+  	            for (int num13 : secuencia1) {
+  	                System.out.print(num13 + " ");
+  	            }
+
+  	            
 	}
-}
+	}
+
+	
 
 
 
