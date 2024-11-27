5887b0f46e7ab3cbde95384f90dabe9f64bd072b
COMPU-S021\daw1 <lailafdezs135@gmail.com>
2024-11-27 12:04:18 +0100

    no message

--- UD3_VideoDaw_LailaFernandez/Cliente/src/Cliente.java
+++ UD3_VideoDaw_LailaFernandez/Cliente/src/Cliente.java
@@ -1,5 +1,59 @@
 public class Cliente {
     public static void main(String[] args) throws Exception {
+            private static int contador = 0;
         
-    }
-}
+            //Atributos
+            private String dni;
+            private String nombre;
+            private String fecha_nacimiento;
+            private int numSocio;
+            private String direccion;
+            private String fecha_baja;
+            private int p_alquiladas;
+        
+            public Cliente(String dni, String nombre, String direccion, String fecha_nacimiento){
+                this.numSocio= this.contador;
+                this.contador++;
+        
+                this.dni = dni;
+                this.nombre = nombre;
+                this.direccion = direccion;
+                this.fecha_nacimiento = "";
+                this.p_alquiladas = 0;
+            }
+            
+            public String getDni() {
+                return dni;
+            }
+            public String getNombre() {
+                return nombre;
+            }
+            public int getNumSocio() {
+                return numSocio;
+            }
+            public String getDireccion() {
+                return direccion;
+            }
+            public String getFechaNacimiento() {
+                return fecha_nacimiento;
+            }
+            public String getFechaBaja() {
+                return fecha_baja;
+            }
+            public int getPalquiladas() {
+                return p_alquiladas;
+            }
+            
+            public String mostrarInfoCliente(){
+                String getInformacionCliente = String.format("Información del cliente" +
+                "DNI: %s, Nombre: %s, Numero de socio: %s, Direccion: %s, Fecha de nacimiento %s, Nº de películas alquiladas %s",  
+                this.dni, this.nombre, this.numSocio, this.direccion, this.fecha_nacimiento, this.p_alquiladas );
+                return mostrarInfoCliente();
+            }
+        
+
+
+        }//main
+    }//class
+     
+
--- UD3_VideoDaw_LailaFernandez/Pelicula/src/Pelicula.java
+++ UD3_VideoDaw_LailaFernandez/Pelicula/src/Pelicula.java
@@ -1,5 +1,63 @@
-public class Pelicula {
-    public static void main(String[] args) throws Exception {
-      
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+    
+    public class Pelicula {
+        
+        private static int contador = 0;
+    
+        //Atributos
+        private int codPeli;
+        private String titulo;
+        private String genero;
+        private String fecha_registro;
+        private String fecha_bajaPeli;
+        private String fecha_alquiler;
+        private boolean isAlquilada;    
+    
+        private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:SS");
+        //Constructores
+        public Pelicula(String titulo, String genero){
+            this.codPeli = this.contador;
+            this.contador++;
+    
+            this.titulo = titulo;
+            this.genero = genero;
+            this.fecha_registro = LocalDateTime.now().format(formatter);
+            this.isAlquilada = false;
+        }
+    
+    
+        public static int getContador() {
+            return contador;
+        }
+        public int getcodPelicula() {
+            return codPeli;
+        }
+        public String getTitulo() {
+            return titulo;
+        }
+        public String getGenero() {
+            return genero;
+        }
+        public String getfechaRegistro() {
+            return fecha_registro;
+        }
+        public String getfechaBaja() {
+            return fecha_bajaPeli;
+        }
+        public String getfechaAlquiler() {
+            return fecha_alquiler;
+        }
+        public boolean isAlquilada() {
+            return isAlquilada;
+        }
+        
+        public String mostrarInfoPelicula(){
+            String getInfoPelicula = String.format("INFO DE LA PELICULA" + 
+            "Cod: Pel-0%s, Titulo: %s, Genero: %s, Registro: %s", 
+            this.codPeli, this.titulo, this.genero, this.fecha_registro);
+            return getInfoPelicula;
+        }
     }
-}
+    
+
