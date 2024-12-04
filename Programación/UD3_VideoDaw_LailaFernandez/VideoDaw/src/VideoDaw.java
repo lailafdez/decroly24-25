a4832cc3020e4cec441e21822f712dc9bf85f70d
COMPU-S021\daw1 <lailafdezs135@gmail.com>
2024-12-04 09:59:14 +0100

    no message

--- Programación/UD3_VideoDaw_LailaFernandez/VideoDaw/src/VideoDaw.java
+++ Programación/UD3_VideoDaw_LailaFernandez/VideoDaw/src/VideoDaw.java
@@ -6,17 +6,19 @@ public class VideoDaw {
     
     //Atributos
     private String cif;
+    private String nombrePeli;
     private String nombreVideoClub;
     private String direccion;
     private Pelicula [] pelis_registradas;
     private int nPelisRegistradas;
     private Cliente [] clientes_registrados;
+    private int nClientesRegistrados;
     private String fechaAlta;
 
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:SS");
 
     //Constructores 
-    public VideoDaw (String cif, String direccion, String nombreVideoClub, int nPelisRegistradas) {
+    public VideoDaw (String cif, String direccion, String nombreVideoClub, int nPelisRegistradas, int nClientesRegistrados) {
         this.cif = cif;
         this.nombreVideoClub = nombreVideoClub;
         this.direccion = direccion;
@@ -24,6 +26,7 @@ public class VideoDaw {
         this.clientes_registrados = new Cliente [100];
         this.fechaAlta = LocalDateTime.now().format(formatter);
         this.nPelisRegistradas = nPelisRegistradas;
+        this.nClientesRegistrados = nClientesRegistrados;
     }
 
     //Getters 
@@ -59,6 +62,15 @@ public class VideoDaw {
         return nPelisRegistradas;
     }
 
+    public int getNumClientesRegistrados() {
+        return nClientesRegistrados;
+    }
+
+    public String getNombrePeli() {
+        return nombrePeli;
+    }
+
+
     //Metodos
     public String mostrarInfoVideoClub() {
         String getMostrarInfoVideoClub = String.format( "INFORMACION DEL VIDEO CLUB: " +
@@ -68,7 +80,28 @@ public class VideoDaw {
     }
 
     public String mostrarPelisRegistradas() {
-        
+        String mostrarInfoPeliRegistrada = String.format("PELICULAS REGISTRADAS: " +
+        "Nombre: %s ",
+        this.nombrePeli );
+        for (int i = 0; i < nPelisRegistradas; i++){
+        mostrarInfoPeliRegistrada += pelis_registradas [i].mostrarInfoPelicula();
+        }
+        return mostrarInfoPeliRegistrada;
+    }
+
+    public String mostrarClientesRegistrados () {
+        String mostrarInfoClientesRegistrados = "";
+        for (int i = 0; i < nClientesRegistrados; i++) {
+            mostrarInfoClientesRegistrados += clientes_registrados [i].mostrarInfoCliente();
+        }
+        return mostrarInfoClientesRegistrados;
+    }
+
+    public String alquilarPeliculas( Pelicula p, Cliente c){
+        String alquilarPelis ="";
+        return alquilarPelis;
     }
 }
+    
+
 
